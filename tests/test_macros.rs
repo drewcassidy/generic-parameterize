8ae56855ec725431841e39329341388de8c922ca
Andrew Cassidy <drewcassidy@me.com>
2022-08-13 18:43:33 -0700

    rename macro back, add support for return types, and add testing

--- tests/test_macros.rs
+++ tests/test_macros.rs
@@ -1,7 +1,40 @@
-use generic_parameterize::test_parameterize;
+use std::any::TypeId;
+use std::collections::HashSet;
+use generic_parameterize::parameterize;
 
+use std::process::{ExitCode, Termination};
 
-#[test_parameterize(A = (String,), B = [42, 1312])]
-fn do_the_thing<A, const B: usize>() {
-    println!("Hello!")
+/// Allows getting data out of a unit test by wrapping it in a Termination.
+/// Played by Arnold Schwarzenegger.
+#[derive(PartialEq, Eq, Hash)]
+pub struct Terminator<T> {
+    pub data: Box<T>,
+}
+
+impl<T> Termination for Terminator<T> {
+    fn report(self) -> ExitCode { ExitCode::SUCCESS }
+}
+
+/// Generates 6 parameterized tests using a matrix of types and constants
+#[parameterize(A = (i32, f32), B = [5, 6, 7])]
+#[test]
+fn test_parameterization_matrix<A: 'static, const B: usize>() -> Terminator<(TypeId, usize)> {
+    return Terminator { data: Box::new((TypeId::of::<A>(), B)) };
+}
+
+/// Tests to make sure the previous test expanded to the correct subtests
+#[test]
+fn metatest_parameterization_matrix() {
+    let mut results: HashSet<_> = test_parameterization_matrix::manifest
+        .iter()
+        .map(|f| *f().data)
+        .collect();
+
+    assert_eq!(results.len(), 6);
+    assert!(results.remove(&(TypeId::of::<i32>(), 5usize)));
+    assert!(results.remove(&(TypeId::of::<i32>(), 6usize)));
+    assert!(results.remove(&(TypeId::of::<i32>(), 7usize)));
+    assert!(results.remove(&(TypeId::of::<f32>(), 5usize)));
+    assert!(results.remove(&(TypeId::of::<f32>(), 6usize)));
+    assert!(results.remove(&(TypeId::of::<f32>(), 7usize)));
 }
